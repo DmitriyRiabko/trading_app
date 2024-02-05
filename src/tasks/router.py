a9867f41693091481bfd3d060260e1a3cf7ba55b
vsegda_vinovat <riabko97@gmail.com>
2024-02-05 15:58:41 +0200

    added router for tasks

---
+++ src/tasks/router.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter
+from fastapi import Depends
+from .tasks import send_email_report_dashboard
+from auth.models import User
+from auth.base_config import current_user
+
+router = APIRouter(
+    prefix='/report'
+)
+
+
+
+
+
+@router.get('/dashboard')
+def get_dashboard_report(user=Depends(current_user)):
+    send_email_report_dashboard.delay(user.username)
+    print('sending mail')
+    return{
+        'status':200,
+        'data':'mail send',
+        'details':None
+    }
+    
+    
+    
