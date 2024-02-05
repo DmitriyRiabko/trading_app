1a515fe2bb6d60ff4cd13d867f994cbff43a11df
vsegda_vinovat <riabko97@gmail.com>
2024-02-05 16:00:25 +0200

    added cred for  SMTP email address

--- src/config.py
+++ src/config.py
@@ -10,5 +10,8 @@ DB_NAME = os.environ.get('DB_NAME')
 DB_USER = os.environ.get('DB_USER')
 DB_PASS = os.environ.get('DB_PASS')
 
+
 SECRET_AUTH = os.environ.get('SECRET_AUTH')
 
+SMTP_USER = os.environ.get('SMTP_USER')
+SMTP_PASS = os.environ.get('SMTP_PASS')
