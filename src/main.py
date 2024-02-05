df0272592df68859d50583e4a2376b9b9af662e1
vsegda_vinovat <riabko97@gmail.com>
2024-02-05 16:01:38 +0200

    added lifespan context manager for app

--- src/main.py
+++ src/main.py
@@ -1,11 +1,38 @@
 from fastapi import  FastAPI
 import fastapi_users
+from contextlib import asynccontextmanager
+from redis import asyncio as aioredis
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+
+
+from operations.router import router as router_operation
 from auth.base_config import auth_backend
+from tasks.router import router as tasks_router
 from auth.schemas import UserCreate, UserRead
 from auth.base_config import fastapi_users
 
+async def startup():
+    redis = aioredis.from_url("redis://localhost")
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+
+
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    await startup()
+    yield
+
+
+
+
+
+
+
+
 app = FastAPI(
-    title="Trading App"
+    title="Trading App",
+    lifespan=lifespan
 )
 
 app.include_router(
@@ -21,3 +48,10 @@ app.include_router(
     tags=["auth"],
 )
 
+app.include_router(router_operation)
+
+
+
+
+app.include_router(tasks_router
+)
