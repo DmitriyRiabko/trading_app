ef96f753f261ddb0cc3b036a435b3efa3ef495bc
vsegda_vinovat <riabko97@gmail.com>
2024-02-08 14:00:18 +0200

    shanges for new model

--- src/operations/router.py
+++ src/operations/router.py
@@ -1,47 +1,40 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select,insert
+from sqlalchemy import select, insert
 import time
+from fastapi import status
 
-from operations.models import operation
+from operations.models import Operation
 from operations.schemas import OperationCreate
 from database import get_async_session
 from fastapi_cache.decorator import cache
 
-router = APIRouter(
-    prefix="/operations",
-    tags=['Operation']
-)
+router = APIRouter(prefix="/operations", tags=["Operation"])
 
 
-@router.get('/long_operation')
+@router.get("/long_operation")
 @cache(expire=30)
 def get_long_op():
     time.sleep(2)
-    return 'to much data'
+    return "to much data"
 
 
-
-
-
-@router.get('/')
+@router.get("/")
 async def get_specific_operation(
-    operation_type:str,
-    session:AsyncSession = Depends(get_async_session)
+    operation_type: str, session: AsyncSession = Depends(get_async_session)
 ):
-    stmt = select(operation).where(operation.c.type == operation_type)
+    stmt = select(Operation).where(Operation.type == operation_type)
     res = await session.execute(stmt)
-    return {'message':res}
+    operations = res.scalars().all()
+    return {"message": operations}
 
 
-@router.post('/')
+@router.post("/")
 async def add_specific_operations(
-    new_operation:OperationCreate,
-    session:AsyncSession =Depends(get_async_session)
-    
+    new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)
 ):
-    
-    stmt = insert(operation).values(new_operation.model_dump())
+
+    stmt = insert(Operation).values(new_operation.model_dump())
     await session.execute(stmt)
     await session.commit()
-    return {'status':'success'}
+    return {"status": status.HTTP_201_CREATED}
