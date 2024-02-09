bc1df5d31023e5ff102044ce559c66f4ebc8db2c
vsegda_vinovat <riabko97@gmail.com>
2024-02-09 15:50:17 +0200

    auth test changes

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -4,33 +4,36 @@ from sqlalchemy import insert, select
 from .conftest import client, async_session_maker
 from src import Role
 
+
 @pytest.mark.asyncio
 async def test_add_role():
     async with async_session_maker() as session:
-        stmt = insert(Role).values(id=2, name="GOD",permissions = None)
-        await session.execute(stmt)
+       
+        session.add(Role(id=8,name='admin',permissions=None))
         await session.commit()
-        
+
         query = select(Role)
         result = await session.execute(query)
         role = result.scalar_one_or_none()
-        
-        # Проверяем, что роль добавлена корректно
-        assert role is not None, 'Role was not added'
-        assert role.id == 2, 'ID does not match'
-        assert role.name == "GOD", 'Name does not match'
-        assert role.permissions is None, 'Permissions do not match'
 
-# def test_register():
-#     client.post(
-#         "/auth/register",
-#         json={
-#             "email": "Zmix@gmail.com",
-#             "password": "123",
-#             "is_active": True,
-#             "is_superuser": False,
-#             "is_verified": False,
-#             "username": "string",
-#             "role_id": 1,
-#         },
-#     )
+        # assert role is not None, "Role was not added"
+        assert role.id == 8, "ID does not match"
+        # assert role.name == "GOD", "Name does not match"
+        # assert role.permissions is None, "Permissions do not match"
+
+@pytest.mark.asyncio
+def test_register():
+    response = client.post(
+        "/auth/register",
+        json={
+            "email": "Z@gmail.com",
+            "password": "123",
+            "is_active": True,
+            "is_superuser": False,
+            "is_verified": False,
+            "username": "string",
+            "role_id": 8,
+        },
+    )
+
+    assert response.status_code == 201
