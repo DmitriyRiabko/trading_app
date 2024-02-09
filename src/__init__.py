731eb3588db337c255dc3eadc0ac38999b709e46
vsegda_vinovat <riabko97@gmail.com>
2024-02-09 15:52:15 +0200

    added new import

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,7 @@
-__all__ = ['User','Operation','Role','DATABASE_URL','Base'],
 
 from auth.models import User, Role
 from operations.models import Operation
-from database import DATABASE_URL, Base
+from database import DATABASE_URL, Base, get_async_session
+# from config import *
+
+__all__ = ['User','Operation','Role','DATABASE_URL','Base','get_async_session',]
